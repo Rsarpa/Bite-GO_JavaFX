d1149eaa6fb09a6eb3e677eb3cf86dbd807d7f3e
PedroGalan23 <pedrogalanagu1@gmail.com>
2025-02-11 06:19:27 +0100

    Diseño Crud de alumnos

--- src/main/java/com/example/bitego_javafx/Controller/AdminController.java
+++ src/main/java/com/example/bitego_javafx/Controller/AdminController.java
@@ -1,9 +1,34 @@
 package com.example.bitego_javafx.Controller;
 
 import com.example.bitego_javafx.Model.Usuario;
+import javafx.fxml.FXML;
 
 public class AdminController {
     private Usuario usuario;
+    @FXML
+    private void añadirAlumno() {
+        // Lógica para abrir un formulario de añadir alumno
+    }
+
+    @FXML
+    private void editarAlumno() {
+        // Lógica para editar el alumno seleccionado
+    }
+
+    @FXML
+    private void borrarAlumno() {
+        // Lógica para eliminar el alumno seleccionado
+    }
+
+    @FXML
+    private void mostrarInformacion() {
+        // Lógica para mostrar detalles del alumno seleccionado
+    }
+
+    @FXML
+    private void filtrarPorNombre() {
+        // Lógica para filtrar los alumnos en la tabla
+    }
 
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
