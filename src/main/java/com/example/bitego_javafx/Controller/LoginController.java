0bc3ca13fc1d2f26333811136200c90701ddd1d2
PedroGalan23 <pedrogalanagu1@gmail.com>
2025-02-11 05:25:29 +0100

    Editar Vista y Tamaño de pantalla

--- src/main/java/com/example/bitego_javafx/Controller/LoginController.java
+++ src/main/java/com/example/bitego_javafx/Controller/LoginController.java
@@ -80,7 +80,7 @@ public class LoginController {
         try{
             //recoger clase del fichero ruta
             FXMLLoader loader = new FXMLLoader(getClass().getResource(ruta));
-            Scene scene = new Scene(loader.load(), 800, 500);
+            Scene scene = new Scene(loader.load(), 1200, 800);
 
             Stage stage = new Stage();
             stage.setTitle("Hello!");
